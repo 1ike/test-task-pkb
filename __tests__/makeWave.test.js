8ec287ce354f3d1ed91914ad8992677bcc2f6a2f
1ike <googlohren@gmail.com>
2018-11-04 22:20:24 +0300

    wave (v.1)

---
+++ __tests__/makeWave.test.js
@@ -0,0 +1,56 @@
+import makeWave, { transformTextToWave } from '../src/makeWave';
+
+const config = {
+  inputSelector: 'input',
+  outputSelector: '.result',
+  startSize: 10,
+  step: 5,
+  measureUnit: 'px',
+};
+
+const testTransform = (text, expected) => {
+  const html = transformTextToWave(text, config);
+
+  expect(html).toBe(expected);
+};
+
+const textABC = 'abc';
+const expectedABC = '<span style="font-size:10px">a</span><span style="font-size:15px">b</span><span style="font-size:10px">c</span>';
+
+
+describe('makeWave', () => {
+  test('odd length', () => {
+    testTransform(textABC, expectedABC);
+  });
+
+  test('even length', () => {
+    const text = 'abcd';
+    const expected = '<span style="font-size:10px">a</span><span style="font-size:15px">b</span><span style="font-size:20px">c</span><span style="font-size:15px">d</span>';
+
+    testTransform(text, expected);
+  });
+
+  test('spaces', () => {
+    const text = ' ab cd ';
+    const expected = '<span style="font-size:10px"> </span><span style="font-size:10px">a</span><span style="font-size:15px">b</span><span style="font-size:15px"> </span><span style="font-size:15px">c</span><span style="font-size:10px">d</span><span style="font-size:10px"> </span>';
+
+    testTransform(text, expected);
+  });
+
+  test('DOM', () => {
+    document.body.innerHTML = `
+      <div><input type="text"></div>
+      <div class="result"></div>
+    `;
+
+    makeWave(config);
+
+    const input = document.querySelector(config.inputSelector);
+    const output = document.querySelector(config.outputSelector);
+    input.value = textABC;
+    input.dispatchEvent(new Event('input'));
+    const received = output.innerHTML;
+
+    expect(received).toBe(expectedABC);
+  });
+});
---
+++ src/makeWave.js
@@ -0,0 +1,89 @@
+/**
+ *  Напишите, пожалуйста, код, который будет преобразовывать строку в волну:
+ *    - Каждый символ, должен быть помещен в span;
+ *    - До середины строки каждый тег span должен иметь
+ *      увеличенный на 5 пикселей шрифт по отношению к предыдущему,
+ *      начиная с 10 пикселей;
+ *    - После середины размер шрифта должен уменьшаться обратно;
+ *    - Пробелы не должны влиять на изменение размера.
+ *
+ *  Для реализации используйте строку из тега input,
+ * результат поместите в тег .result:
+ *  <div><input type="text"></div>
+ *  <div class="result"></div>
+ */
+
+/* const config = {
+  inputSelector: 'input',
+  outputSelector: '.result',
+  startSize: 10,
+  step: 5,
+  measureUnit: 'px',
+}
+ */
+const getSupportedEvent = (elem, eventsNames) => {
+  for (let i = 0; i < eventsNames.length; i++) {
+    if (`on${eventsNames[i].trim().toLowerCase()}` in elem) {
+      return eventsNames[i];
+    }
+  }
+};
+
+
+const wrapInSpan = (char, fontSize) => `<span style="font-size:${fontSize}">${char}</span>`;
+
+export const transformChar = (state, char, idx, chars) => {
+  const {
+    transformedChars,
+    size,
+    step,
+    measureUnit,
+  } = state;
+
+  const newChar = wrapInSpan(char, `${size}${measureUnit}`);
+
+  let newSize = size;
+  if (char !== ' ' && chars[idx + 1] !== ' ') {
+    newSize = idx + 1 <= chars.length / 2 ? size + step : size - step;
+  }
+
+  return {
+    ...state,
+    size: newSize,
+    transformedChars: [...transformedChars, newChar],
+  };
+};
+
+export const transformTextToWave = (text, config) => {
+  const chars = text.split('');
+
+  const initState = {
+    transformedChars: [],
+    size: config.startSize,
+    step: config.step,
+    measureUnit: config.measureUnit,
+  };
+
+  const { transformedChars } = chars.reduce(transformChar, initState);
+
+  return transformedChars.join('');
+};
+
+
+const makeWave = (config) => {
+  const { inputSelector, outputSelector } = config;
+
+  const input = document.querySelector(inputSelector);
+  const output = document.querySelector(outputSelector);
+
+  const events = ['input', 'change', 'keyup', 'blur'];
+  const supportedEvent = getSupportedEvent(input, events);
+
+  input.addEventListener(supportedEvent, () => {
+    const wave = transformTextToWave(input.value, config);
+    // or input.value.trim()
+    output.innerHTML = wave;
+  });
+};
+
+export default makeWave;
